051e56bd8a6c55f909cfc4d742e16712b8412ef4
Marco Granelli <marco.granelli@ymail.com>
2023-02-03 17:07:09 +0100

    Tx expiration validation

--- apps/src/lib/node/ledger/shell/mod.rs
+++ apps/src/lib/node/ledger/shell/mod.rs
@@ -130,6 +130,7 @@ pub enum ErrorCodes {
     ReplayTx = 7,
     InvalidChainId = 8,
     InvalidDecryptedChainId = 9,
+    ExpiredTx = 10,
 }
 
 impl From<ErrorCodes> for u32 {
@@ -610,6 +611,24 @@ where
                 self.chain_id, tx.chain_id
             );
             return response;
+        }
+
+        // Tx expiration
+        if let Some(exp) = tx.expiration {
+            let last_block_timestamp = self
+                .wl_storage
+                .storage
+                .get_block_timestamp()
+                .expect("Failed to retrieve last block timestamp");
+
+            if exp > last_block_timestamp {
+                response.code = ErrorCodes::ExpiredTx.into();
+                response.log = format!(
+                    "Tx expired at {:#?}, last committed block time: {:#?}",
+                    exp, last_block_timestamp
+                );
+                return response;
+            }
         }
 
         // Tx signature check
@@ -1145,7 +1164,6 @@ mod test_utils {
 #[cfg(test)]
 mod test_mempool_validate {
     use namada::proto::SignedTxData;
-    use namada::types::storage::Epoch;
     use namada::types::transaction::{Fee, WrapperTx};
 
     use super::test_utils::TestShell;
